136ec4f92ef347e30f134669c6bcd16ee09fa992
banshika03 <banshikachoithani2016@gmail.com>
2024-07-04 20:47:39 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,10 +1,11 @@
 const accountId=2345
-let accountEmail = "anushka@google"
+let accountEmail = "banshika@google"
 var password = "12345"
 accountcity  = "Jaipur"
 accountEmail="hc@hchc.com"
 password="212211"
 accountcity="bengluru"
 //accountId =2
+let accountScope;
 console.log(accountId);
-console.table([accountId,accountEmail,password,accountcity])
+console.table([accountId,accountEmail,password,accountcity,accountScope])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict";
+//alert( 3+3)
+console.log(3+3);
+console.log("banshika");
+let name="bansh"
+let age=20
+let islogedin=true
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null);
