3c4fa63b4a887ee57136dc3b39d07517d46763b3
banshika03 <banshikachoithani2016@gmail.com>
2024-07-04 19:50:14 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=2345
+let accountEmail = "anushka@google"
+var password = "12345"
+accountcity  = "Jaipur"
+accountEmail="hc@hchc.com"
+password="212211"
+accountcity="bengluru"
+//accountId =2
+console.log(accountId);
+console.table([accountId,accountEmail,password,accountcity])
