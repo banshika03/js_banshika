a006f1c33d9ebb1f7706c9d67bb8d6cd5b9550e1
banshika03 <banshikachoithani2016@gmail.com>
2024-07-06 18:37:52 +0000

    conversion of datatypes

---
+++ 01_basics/03_con.js
@@ -0,0 +1,41 @@
+let score= "banshika"
+console.log(typeof score);
+console.log(typeof (score))
+let valueINnumber = Number(score)
+console.log(typeof valueINnumber);
+console.log(valueINnumber);
+
+/* "33" => 33
+"33abc" => NaN
+ true=1; false=0;
+*/
+// boolean to string and number
+let isLoggedIn =true
+console.log(typeof isLoggedIn)
+let xy=Number(isLoggedIn)
+console.log(typeof xy)
+console.log(xy)
+// string to boolean
+let ab=""
+console.log(ab);
+console.log(typeof ab);
+let  bool = Boolean(ab)
+console.log(bool);
+console.log(typeof bool);
+//number to boolean
+let num=1
+console.log(num);
+console.log(typeof num);
+let boole = Boolean(num)
+console.log(boole);
+console.log(typeof boole);
+
+//number to string
+let numb = 23
+console.log(numb);
+console.log(typeof numb);
+let stg= String(numb)
+console.log(stg);
+console.log(typeof stg);
+
+
