c727d01c8d82984c776d2c3f362b45bd1872c120
banshika03 <banshikachoithani2016@gmail.com>
2024-07-08 18:39:00 +0000

    datatypes summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,55 @@
+/* primitive datatypes
+original datatypes
+7 types :
+1. string
+2. number 
+3. boolean
+4. null
+5. undefined
+6. symbol
+7. BigInt */
+
+const ID= Symbol('123')
+const anotherID= Symbol('123')
+console.log( ID==anotherID);
+
+//let bignumber=123456789062n
+//console.log(typeof bignumber);
+
+//reference type(non primitive):DATATYPES WHO ARE  MADE UP OF MULTIPLE DATATYPES
+
+
+
+//array, functions, objects
+
+const cartoons=["anushka","shinchan"];
+let myobj={
+    name:"banshika",
+    age:20,
+}
+const myfunction= function(){
+    console.log("hello");
+}
+
+console.log(typeof bignumber);
+const temp=null
+console.log(typeof temp);
+console.log(typeof myfunction);
+
+/*Primitive datatypes:
+String - string
+Number - number
+Boolean - Boolean
+Null - object(****)
+Undefined - undefined
+BigInt - bigint
+
+Non-primitive datatypes:
+Array - object
+Function - function(object)
+Object - object */
+
+//--------------------------------------------------------------------------------------
+ 
+//types of memory
+//stack(primitive), heap(non prinitive)
